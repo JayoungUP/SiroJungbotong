126374f53923be3fd89a1e4fe7d97d6b8d1764c1
1233day <1233day@naver.com>
2025-05-25 14:21:51 +0900

    init: backend

---
+++ backend/src/main/kotlin/com/jayoungup/sirojungbotong/SirojungbotongApplication.kt
@@ -0,0 +1,11 @@
+package com.jayoungup.sirojungbotong
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SirojungbotongApplication
+
+fun main(args: Array<String>) {
+	runApplication<SirojungbotongApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/jayoungup/sirojungbotong/SirojungbotongApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jayoungup.sirojungbotong
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SirojungbotongApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
