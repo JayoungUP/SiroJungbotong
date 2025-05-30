c18e5a045a569905ff12576557845108fc9a3bae
Evan Choi (최성현) <111727754+froggy-hyun@users.noreply.github.com>
2025-05-30 19:15:41 +0900

    Revert "(#2)BE-feat/auth-signup-login"

--- backend/src/main/kotlin/com/jayoungup/sirojungbotong/Member/dto/request/UserSignupRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.jayoungup.sirojungbotong.Member.dto.request
-
-data class UserSignupRequest (
-    val loginId: String,
-    val email: String,
-    val name: String,
-    val password: String,
-    val nickname: String,
-)
