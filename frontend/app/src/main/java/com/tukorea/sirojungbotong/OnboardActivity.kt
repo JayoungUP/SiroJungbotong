dc514977d2a6561b916b0a9d907ca8978080d532
geonwooj <163291251+geonwooj@users.noreply.github.com>
2025-06-01 16:24:18 +0900

    onboard.kt

---
+++ frontend/app/src/main/java/com/tukorea/sirojungbotong/OnboardActivity.kt
@@ -0,0 +1,53 @@
+package com.david.siro
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.david.siro.databinding.OnBoardBinding
+import kotlin.jvm.java
+
+class OnboardActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val bind= OnBoardBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+        bind.OnboardSearch.setOnClickListener {
+            try{
+                val intent=Intent(this,MarketListActivity::class.java)
+                startActivity(intent)
+            }
+            catch(e:Exception){
+                Toast.makeText(this, "MarketListActivity를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        bind.Upload.setOnClickListener {
+            try{
+                val intent=Intent(this,MarketUploadActivity::class.java)
+                startActivity(intent)
+            }
+            catch(e:Exception){
+                Toast.makeText(this, "MarketListActivity를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+            bind.OnboardLogin.setOnClickListener {
+                // 예시: LoginActivity 로 이동
+                try {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    Toast.makeText(this, "LoginActivity를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}}
