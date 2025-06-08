124bbe8702352bd7a8dd17ba979168080128ea29
sgtj7 <sgtj78@tukorea.ac.kr>
2025-06-08 19:45:51 +0900

    feat:전단지 API와 연동 완료

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -58,6 +58,10 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
