f5ddd43b55aade4a57afd22613baaeb93d6f7ed2
sgtj7 <sgtj78@tukorea.ac.kr>
2025-06-15 09:32:11 +0900

    feat:전단지 API와 연동 완료

--- frontend/gradle/build.gradle.kts
+++ frontend/gradle/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -6,6 +8,14 @@ plugins {
 android {
     namespace = "com.tukorea.sirojungbotong"
     compileSdk = 35
+    defaultConfig {
+        applicationId = "com.tukorea.sirojungbotong"
+        minSdk = 21
+        targetSdk = 35      // 타겟 SDK
+        versionCode = 1
+        versionName = "1.0"
+    }
+
     viewBinding {
         enable=true
     }
@@ -36,16 +46,26 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    viewBinding{
+        enable = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
     testImplementation(libs.junit)
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
